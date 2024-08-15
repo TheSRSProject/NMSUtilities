5884e8aa528fbe6de42f87312c4d543f8d29c84a
ZX夏夜之风 <snwcreations@qq.com>
2024-08-16 00:08:40 +0800

    feat: get player head

--- common/src/main/java/snw/srs/nms/NMSUtilitiesAdapter.java
+++ common/src/main/java/snw/srs/nms/NMSUtilitiesAdapter.java
@@ -1,7 +1,13 @@
 package snw.srs.nms;
 
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+import java.util.concurrent.CompletableFuture;
 
 public interface NMSUtilitiesAdapter {
     void showCreditScreen(Player player);
+
+    CompletableFuture<ItemStack> getPlayerHead(Player player, Plugin requester);
 }
