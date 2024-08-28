d9906eac5402fd758fcc68cba4be781fecfbac7a
ZX夏夜之风 <snwcreations@qq.com>
2024-08-28 19:53:46 +0800

    fix: rewrite version extractor

--- main/src/main/java/snw/srs/nms/AdapterRetriever.java
+++ main/src/main/java/snw/srs/nms/AdapterRetriever.java
@@ -4,28 +4,41 @@ import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import snw.srs.nms.impl.V1_20_R1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @SuppressWarnings("unused")
 public final class AdapterRetriever {
+
+    public static final String MINECRAFT_VERSION;
+    public static final NMSUtilitiesAdapter ADAPTER;
+
+    private static final Pattern VERSION_PATTERN = Pattern.compile(".*\\(.*MC.\\s*([a-zA-z0-9\\-.]+).*");
+
     private AdapterRetriever() {
     }
 
-    public static final String SERVER_VERSION;
-    public static final NMSUtilitiesAdapter ADAPTER;
-
     static {
-        final Server craftServer = Bukkit.getServer();
-        final Class<? extends Server> serverClass = craftServer.getClass();
-        final String packageName = serverClass.getPackageName();
-        final int lastIndexOf = packageName.lastIndexOf(".");
-        SERVER_VERSION = packageName.substring(lastIndexOf + 1);
-
-        // we may add support of other Minecraft version in the future?
+        Server server = Bukkit.getServer();
+        String version = server.getVersion();
+        MINECRAFT_VERSION = extractVersion(version);
+        // The switch statement must have at least 1 result
+        // Don't forget to add the adapter NEW statements there if you supported more version
+        // Your plugin may support multiple Minecraft version
         // noinspection SwitchStatementWithTooFewBranches
-        switch (SERVER_VERSION) {
-            case "v1_20_R1" -> {
-                ADAPTER = new V1_20_R1();
-            }
-            default -> throw new IllegalArgumentException("Unsupported server version " + SERVER_VERSION);
+        ADAPTER = switch (MINECRAFT_VERSION) {
+            case "1.20.1" -> new V1_20_R1();
+            default -> throw new IllegalStateException("Unsupported Minecraft version: " + MINECRAFT_VERSION);
+        };
+    }
+
+    private static String extractVersion(String text) {
+        Matcher version = VERSION_PATTERN.matcher(text);
+
+        if (version.matches() && version.group(1) != null) {
+            return version.group(1);
+        } else {
+            throw new IllegalStateException("Cannot parse version String '%s'".formatted(text));
         }
     }
 
